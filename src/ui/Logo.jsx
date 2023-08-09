e4dde1e5a918548489f7e6976bedf909e1fc583e
Emilio Pino <pinoen@yahoo.com.ar>
2023-08-08 22:00:18 -0300

    change logo

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -13,7 +13,7 @@ const Img = styled.img`
 function Logo() {
   const { isDarkMode } = useDarkMode();
 
-  const src = isDarkMode ? "/logo-dark.png" : "/logo-light.png";
+  const src = isDarkMode ? "/pinoen-light.svg" : "/pinoen-dark.svg";
 
   return (
     <StyledLogo>
